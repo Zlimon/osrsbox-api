e0af9587d296870262e951dd983b6cf004589069
Zlimon <simon@lonnestad.no>
2024-07-14 00:51:18 +0200

    Use mongosh instead of mongo

--- mongo/create-database.sh
+++ mongo/create-database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mongo -- "osrsbox-db" <<EOF
+mongosh -- "osrsbox-db" <<EOF
     var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
     var rootPassword = '$MONGO_INITDB_ROOT_PASSWORD';
     var admin = db.getSiblingDB('admin');
@@ -12,10 +12,10 @@ mongo -- "osrsbox-db" <<EOF
     db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
 EOF
 
-mongo -- "osrsbox-db" <<EOF 
+mongosh -- "osrsbox-db" <<EOF
     db.temp.insertOne(
-        { 
-            random: "JIfxp2mVSz61ZF7qzgmF4vBDd2eHjKSiZV6WzUXr24E=", 
+        {
+            random: "JIfxp2mVSz61ZF7qzgmF4vBDd2eHjKSiZV6WzUXr24E=",
         }
     );
 EOF
